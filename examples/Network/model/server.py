54bf05ed8ee4cfeeb2668c124abd1ff169e652e4
jackiekazil <jackiekazil@gmail.com>
2016-05-04 00:50:11 -0400

    Minor updates.

--- examples/Network/model/server.py
+++ examples/Network/model/server.py
@@ -6,35 +6,42 @@ from mesa import Agent, Model
 
 class VirusAgent(Agent):
 
-    def __init__(self, infected, resistant):
+    def __init__(self, infected=False, resistant=False):
 
-        self.infected = False
-        self.resistant = False
+        self.infected = infected
+        self.resistant = resistant
 
 
 class VirusModel(Model):
 
-    def __init__(self, N, G=nx.Graph(), avg_node_degree=6):
+    # setting average node degree to '6' <-- TODO turn to slider
+    def __init__(self, N, G=None, avg_node_degree=6):
         self.num_agents = N
-        self.graph = G
 
+        if not G:
+            self.graph = self._create_network()
+        else:
+            self.graph = G
+
+    # TODO: Discuss - maybe this is abstracted out / generalized?
+    def _create_network(self):
+
+        G = nx.Graph()
         for i in range(self.num_agents):
             # create nodes
             a = VirusAgent(i)
             G.add_node(a)
 
-        # create edges
-        # setting average node degree to '6' <-- TODO turn to slider
-
-        num_links = (avg_node_degree * self.num_agents / 2)
+        num_links = (self.avg_node_degree * self.num_agents / 2)
         while len(G.edges()) <= num_links:
-            first_node = choice(G.nodes())
+            firstn = choice(G.nodes())
             possible_nodes = G.nodes()
-            possible_nodes.remove(first_node)
-            second_node = choice(possible_nodes)
+            possible_nodes.remove(firstn)
+            secondn = choice(possible_nodes)
 
-            # If there is no edge create an edge.
-            # ^^ Turn this into a function that finds two nodes that are unlinked
+            edges = G.edges()
 
-
-
+            # TODO: Rewrite for directed graphs
+            # add a way for this to be handled.
+            if (firstn, secondn) not in edges and (secondn, firstn):
+                G.add_edge(firstn, secondn)
